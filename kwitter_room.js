214ea8944ffd5f39ef9bf1209b06c7f888b7a6b8
Ravi51234 <140091754+Ravi51234@users.noreply.github.com>
2023-12-25 13:35:05 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDd5UV1LGWliT2sK7_VYnG0shH3JOmEQBI",
+    authDomain: "lets-chat-28507.firebaseapp.com",
+    projectId: "lets-chat-28507",
+    storageBucket: "lets-chat-28507.appspot.com",
+    messagingSenderId: "761884647041",
+    appId: "1:761884647041:web:6f0ad898f13eb659b7b780",
+    measurementId: "G-ZXNV3Z6E6Z"
+  };
